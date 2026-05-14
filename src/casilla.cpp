383743103bcafd262c8c409b05e27f3b8c56ee65
Sergio Diaz Pulido <sergio.diazpulido03@gmail.com>
2026-05-14 19:37:28 +0200

    Commit 6: Creacion e integracion del tablero en el main y configuracion de camara 2D

--- src/casilla.cpp
+++ src/casilla.cpp
@@ -1,6 +1,6 @@
-#include "casilla.h"
+#include "Casilla.h"
+#include "freeglut.h" // Librería necesaria para dibujar
 
-// Constructor: por defecto la hacemos blanca (1, 1, 1 en OpenGL)
 Casilla::Casilla() {
     rojo = 1.0f;
     verde = 1.0f;
@@ -15,5 +15,18 @@ void Casilla::setColor(float r, float g, float b) {
     azul = b;
 }
 
-// Aquí meteremos el código de OpenGL (GL_QUADS) más adelante
-void Casilla::dibuja() {}
+void Casilla::dibuja() {
+    // Definimos el tamaño de la casilla (un cuadrado de lado 1)
+    float lado = 1.0f;
+
+    // Le decimos a OpenGL el color de esta casilla específica
+    glColor3f(rojo, verde, azul);
+
+    // Dibujamos el cuadrado usando las coordenadas de su posición
+    glBegin(GL_QUADS);
+    glVertex2f(pos.x, pos.y);              // Esquina inferior izquierda
+    glVertex2f(pos.x + lado, pos.y);       // Esquina inferior derecha
+    glVertex2f(pos.x + lado, pos.y + lado);// Esquina superior derecha
+    glVertex2f(pos.x, pos.y + lado);       // Esquina superior izquierda
+    glEnd();
+}
