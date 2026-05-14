a1129ca9081c55f0c5ffc4cd69870f8c9745c39b
Sergio Diaz Pulido <sergio.diazpulido03@gmail.com>
2026-05-14 15:39:46 +0200

    Commit 4: Creacion de la clase Casilla para el tablero

---
+++ src/casilla.cpp
@@ -0,0 +1,19 @@
+#include "Casilla.h"
+
+// Constructor: por defecto la hacemos blanca (1, 1, 1 en OpenGL)
+Casilla::Casilla() {
+    rojo = 1.0f;
+    verde = 1.0f;
+    azul = 1.0f;
+}
+
+Casilla::~Casilla() {}
+
+void Casilla::setColor(float r, float g, float b) {
+    rojo = r;
+    verde = g;
+    azul = b;
+}
+
+// Aquí meteremos el código de OpenGL (GL_QUADS) más adelante
+void Casilla::dibuja() {}
